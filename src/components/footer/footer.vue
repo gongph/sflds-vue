036a571a0431749a961435dd674b9d8be75386de
gongph <gongph@foxmail.com>
2017-06-09 14:20:33 +0800

    footer component

---
+++ src/components/footer/footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="page_bottom">
+    <slot></slot>
+    <br>
+    {{copyright}}
+    <br>
+    {{address}}
+  </div>
+</template>
+<script>
+  export default {
+    props: {
+      copyright: {
+        type: String,
+        default: '版权所有：2016年 中国证监会'
+      },
+      address: {
+        type: String,
+        default: '地址：西城区金融大街3号金益大厦'
+      }
+    }
+  }
+</script>
