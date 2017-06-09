2f0e65e668593f25dabc9fd90c4305bac0768977
gongph <gongph@foxmail.com>
2017-06-09 14:23:23 +0800

    quick-link component

---
+++ src/components/quick-link/quick-link.vue
@@ -0,0 +1,19 @@
+<script>
+  export default {
+    render: function (c) {
+      var h5El, self = this;
+      h5El = c('h5', {}, self.title);
+      return c('div', {
+        class: {
+          'quick_links': true
+        }
+      }, [h5El, self.$slots.default]);
+    },
+    props: {
+      title: {
+        type: String,
+        default: '快速链接'
+      }
+    }
+  }
+</script>
