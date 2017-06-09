6fb4fbb0c5cf0fb4dc856103eedd9964865dc357
gongph <gongph@foxmail.com>
2017-06-09 14:19:25 +0800

    breadcrumb component

---
+++ src/components/breadcrumb/breadcrumb-item.vue
@@ -0,0 +1,38 @@
+<script>
+  import LinkMixin from '../../mixins/link.vue';
+  export default {
+  	name: 'BreadcrumbItem',
+    render: function (c) {
+      var spanEl, linkEl, self = this;
+      linkEl = c('a', {
+        attrs: self.attrsObject
+      }, [self.$slots.default]);
+
+      if (!self.showSeparator) {
+        spanEl = c('span', {
+          class: {
+            'breadcrumb-item-separator': true
+          },
+          style: {
+            margin: '0 8px'
+          },
+        }, self.separator);
+      }
+      else {
+        spanEl = c('span', {}, [self.$slots.separator]);
+      }
+
+      return c('span', {}, [linkEl, spanEl]);
+    },
+    data () {
+      return {
+        separator: '',
+        showSeparator: false
+      }
+    },
+    mixins: [LinkMixin],
+    mounted () {
+      this.showSeparator = this.$slots.separator !== undefined;
+    }
+  }
+</script>
---
+++ src/components/breadcrumb/breadcrumb.vue
@@ -0,0 +1,33 @@
+<script>
+  export default {
+  	name: 'Breadcrumb',
+    render: function (c) {
+      var self = this;
+      return c('div', {
+        class: {
+          'position_nav_l': true
+        }
+      }, [self.title, self.$slots.default]);
+    },
+    props: {
+      title: {
+        type: String,
+        default: '您所在的位置：'
+      },
+      separator: {
+      	type: String,
+      	default: '>'
+      }
+    },
+    mounted () {
+      this.updateChildren();
+    },
+    methods: {
+      updateChildren () {
+        this.$children.forEach((child) => {
+          child.separator = this.separator;
+        });
+      }
+    }
+  }
+</script>
