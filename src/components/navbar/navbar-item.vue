5ce5c5b9ab496d9117c53e15851e64bdbdf14d7f
gongph <gongph@foxmail.com>
2017-06-09 14:22:29 +0800

    navbar component

---
+++ src/components/navbar/navbar-item.vue
@@ -0,0 +1,23 @@
+<script>
+  import LinkMixin from '../../mixins/link.vue';
+  export default {
+    render: function (c) {
+      var self = this;
+      return c('a', {
+        class: {
+          'active_nav': self.active ? true : false
+        },
+        attrs: self.attrsObject,
+        on: {
+          click: self.onClick
+        }
+      }, self.text);
+    },
+    mixins: [LinkMixin],
+    methods: {
+      onClick (event) {
+        this.$emit('click', event);
+      }
+    }
+  }
+</script>
---
+++ src/components/navbar/navbar.vue
@@ -0,0 +1,10 @@
+<script>
+  export default {
+    render: function (c) {
+      var self = this;
+      return c('div', {
+        class: 'main_nav'
+      }, [self.$slots.default]);
+    }
+  }
+</script>
