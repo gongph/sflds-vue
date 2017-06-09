1230f56d85ca161023442de1427dac367b95bc1b
gongph <gongph@foxmail.com>
2017-06-09 14:24:56 +0800

    view component

---
+++ src/components/view/view.vue
@@ -0,0 +1,12 @@
+<script>
+  export default {
+    render: function (c) {
+      var self = this;
+      return c('div', {
+        class: {
+          'pager_m': true
+        }
+      }, [self.$slots.default])
+    }
+  }
+</script>
