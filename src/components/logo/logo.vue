f925aae1afed54c56be327e25dfc2be54d634dbc
gongph <gongph@foxmail.com>
2017-06-09 14:21:33 +0800

    logo component

---
+++ src/components/logo/logo.vue
@@ -0,0 +1,23 @@
+<script>
+  export default {
+  	data () {
+  	  return {
+  	    sysCn: '法规数据库查询系统',
+  	    sysEn: 'Regulatory database query system'
+  	  }
+  	},
+    render: function (c) {
+      var syscnEl, sysenEl, self = this;
+      syscnEl = c('p', { class: { 'sys_cn': true } }, self.sysCn);
+      sysenEl = c('p', { class: { 'sys_en': true } }, self.sysEn);
+
+      var logoEl = c('div', {
+        class: {
+          'page_header_logo': true
+        }
+      }, [syscnEl, sysenEl]);
+
+      return logoEl;
+    }
+  }
+</script>
