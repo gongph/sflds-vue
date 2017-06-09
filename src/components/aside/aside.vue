5e829f7d1cefa3e80da81d05eceb6cc35c479f49
gongph <gongph@foxmail.com>
2017-06-09 14:19:03 +0800

    aside component

---
+++ src/components/aside/aside.vue
@@ -0,0 +1,20 @@
+<script>
+  export default {
+    render: function (c) {
+      var self = this;
+      return c('div', {
+        class: {
+          [this.alignClass]: true
+        }
+      }, [self.$slots.default]);
+    },
+    props: {
+      align: String
+    },
+    data () {
+      return {
+        alignClass: this.align + '_main'
+      }
+    }
+  }
+</script>
