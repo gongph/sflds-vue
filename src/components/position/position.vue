fb5b7294c8c5290e4f2b295b7779fe8abadb798b
gongph <gongph@foxmail.com>
2017-06-09 14:23:07 +0800

    position component

---
+++ src/components/position/position.vue
@@ -0,0 +1,12 @@
+<script>
+  export default {
+    render: function (c) {
+      var self = this;
+      return c('div', {
+        class: {
+          'position_nav': true
+        }
+      }, [self.$slots.default]);
+    }
+  }
+</script>
