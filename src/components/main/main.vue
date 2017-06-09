493f6da4af234d1512e214f481b77507d24139fe
gongph <gongph@foxmail.com>
2017-06-09 14:22:03 +0800

    main component

---
+++ src/components/main/main.vue
@@ -0,0 +1,18 @@
+<script>
+  export default {
+    render: function (c) {
+      var self = this;
+      return c('div', {
+        class: {
+          [this.classes]: true
+        }
+      }, [self.$slots.default]);
+    },
+    props: {
+      classes: {
+        type: String,
+        default: 'page_middle'
+      }
+    }
+  }
+</script>
