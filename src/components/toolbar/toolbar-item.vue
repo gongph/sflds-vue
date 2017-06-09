231550e69e00ec7fe9bdec30cec49af1fa6f832b
gongph <gongph@foxmail.com>
2017-06-09 14:24:40 +0800

    toolbar component

---
+++ src/components/toolbar/toolbar-item.vue
@@ -0,0 +1,22 @@
+<script>
+  import LinkMixin from '../../mixins/link.vue'
+  export default {
+    render: function (c) {
+      var linkEl, self = this;
+      linkEl = c('a', {
+        attrs: self.attrsObject,
+        on: {
+          click: self.onClick
+        }
+      }, self.text);
+
+      return c('li', {}, [linkEl]);
+    },
+    mixins: [LinkMixin],
+    methods: {
+      onClick (event) {
+        this.$emit('click', event);
+      }
+    }
+  }
+</script>
---
+++ src/components/toolbar/toolbar.vue
@@ -0,0 +1,13 @@
+<script>
+  export default {
+    render: function (c) {
+      var ulEl, self = this;
+      ulEl = c('ul', {}, [self.$slots.default])
+      return c('div', {
+        class: {
+          'sub_info': true
+        }
+      }, [ulEl])
+    }
+  }
+</script>
