9e4b0ebaede159ef995f8827a7795ddd3f77429d
gongph <gongph@foxmail.com>
2017-06-09 14:18:21 +0800

    add mixins vue files

---
+++ src/mixins/link.vue
@@ -0,0 +1,28 @@
+<script>
+  export default {
+    props: {
+      href: {
+        type: String,
+        default: '#'
+      },
+      text: String,
+      active: Boolean,
+      target: {
+        type: String,
+        default: '_blank'
+      }
+    },
+    computed: {
+      attrsObject: function () {
+        var self = this;
+        var ao = {
+          href: self.href
+        };
+
+        if (self.target) ao['target'] = self.target;
+
+        return ao;
+      }
+    }
+  }
+</script>
---
+++ src/mixins/page.vue
@@ -0,0 +1,21 @@
+<script>
+  export default {
+    props: {
+      // 当前页
+      current: {
+        type: Number,
+        default: 1
+      },
+      // 每页显示条数
+      pageSize: {
+        type: Number,
+        default: 20
+      },
+      // 是否显示跳转
+      showJump: {
+      	type: Boolean,
+      	default: true
+      }
+    }
+  }
+</script>
