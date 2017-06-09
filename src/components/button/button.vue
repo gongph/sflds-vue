24620413e427f907b2c5cb3349a5b1d8ed988952
gongph <gongph@foxmail.com>
2017-06-09 14:19:47 +0800

    button component

---
+++ src/components/button/button.vue
@@ -0,0 +1,26 @@
+<script>
+  export default {
+  	props: {
+  	  text: String
+  	},
+    render: function (c) {
+      var buttonEl, self = this;
+      buttonEl = c('button', {
+        class: {
+          'btn': true,
+          'btn-primary': true
+        },
+        on: {
+          click: self.onClick
+        }
+      }, [self.$slots.default]);
+
+      return buttonEl;
+    },
+    methods: {
+      onClick (event) {
+        this.$emit('click', event);
+      }
+    }
+  }
+</script>
