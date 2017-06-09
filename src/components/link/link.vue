e67c7a778ad740215da2e6e7b18953b03dc8d184
gongph <gongph@foxmail.com>
2017-06-09 14:21:16 +0800

    link component

---
+++ src/components/link/link.vue
@@ -0,0 +1,12 @@
+<script>
+  import LinkMixin from '../../mixins/link.vue';
+  export default {
+    render: function (c) {
+      var self = this;
+      return c('a', {
+      	attrs: self.attrsObject
+      }, [self.$slots.default]);
+    },
+    mixins: [LinkMixin]
+  }
+</script>
