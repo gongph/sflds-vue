e6d40be065eda047828cac142e98839df2649b09
gongph <gongph@foxmail.com>
2017-06-09 14:20:53 +0800

    header component

---
+++ src/components/header/header.vue
@@ -0,0 +1,14 @@
+<script>
+  export default {
+    render: function (c) {
+      var logoEl, self = this;
+      logoEl = c('sf-logo')
+
+      return c('div', {
+        class: {
+          'page_header': true
+        }
+      }, [logoEl, self.$slots.default])
+    }
+  }
+</script>
