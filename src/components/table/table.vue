be674cbf329b3ac6933b63f7296842b724253204
gongph <gongph@foxmail.com>
2017-06-09 14:24:03 +0800

    table component

---
+++ src/components/table/table.vue
@@ -0,0 +1,94 @@
+<template>
+  <div>
+  <template v-if="showPage">
+    <Page :current="currentPage" 
+          :pageSize="pageSize" 
+          :total="cloneData.length"  
+          @on-change="onPageChange"></Page>
+  </template>
+  <table class="table table-hover table-striped table-condensed" :class="classes">
+    <colgroup>
+      <col v-for="column in columns" :width="column.width">
+    </colgroup>
+    <thead>
+      <tr><th v-for="column in columns" :class="column.classes" v-text="column.title"></th></tr>
+    </thead>
+    <tbody>
+      <template v-for="row in cloneData">
+        <tr><td v-for="column in columns" 
+              v-html="renderTd(row, column)" 
+              :key="row" 
+              :class="column.classes"></td>
+        </tr>
+      </template>
+    </tbody>
+  </table>
+  <template v-if="showPage">
+    <Page :current="currentPage" 
+          :pageSize="pageSize" 
+          :total="cloneData.length" 
+          @on-change="onPageChange"></Page>
+  </template>
+</template>
+</div>
+<script>
+  import Assist from '../../utils/assist.js';
+  import Page from '../page/page.vue';
+  import PageMixin from '../../mixins/page.vue';
+  export default {
+    name: 'table',
+    components: { Page },
+    mixins: [PageMixin],
+    props: {
+      // 自定义样式
+      classes: String,
+      // 是否显示分页
+      showPage: {
+        type: Boolean,
+        default: true
+      },
+      // 列
+      columns: {
+        type: Array,
+        default () {
+          return [];
+        }
+      },
+      // 服务端数据
+      data: {
+        type: Array,
+        default () {
+          return [];
+        }
+      }
+    },
+    data () {
+      return {
+      	currentPage: this.current,
+        cloneColumns: Assist.deepCopy(this.columns),
+        cloneData: Assist.deepCopy(this.data)
+      }
+    },
+    watch: {
+      data () {
+        this.cloneData = Assist.deepCopy(this.data);
+      },
+      columns () {
+        this.cloneColumns = Assist.deepCopy(this.columns);
+      }
+    },
+    methods: {
+      renderTd (row, column) {
+        if ('render' in column && Assist.typeof(column.render) === 'function') {
+          return column.render(row[column.key], row);
+        } else {
+          return row[column.key];
+        }
+      },
+      onPageChange (pageInfo) {
+        this.currentPage = pageInfo.page;
+        this.$emit('on-page-change', pageInfo)
+      }
+    }
+  }
+</script>
