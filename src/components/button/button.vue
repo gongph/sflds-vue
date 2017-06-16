1618c40ced41b262df91e31c08e26fe49096c5ae
gongph <gongph@foxmail.com>
2017-06-16 17:29:56 +0800

    fix button.vue

--- src/components/button/button.vue
+++ src/components/button/button.vue
@@ -1,18 +1,39 @@
 <script>
   export default {
     render: function (c) {
-      var buttonEl, self = this;
+      var buttonEl, tagEl, self = this;
+      
       buttonEl = c('button', {
-        class: {
-          'btn': true,
-          'btn-primary': true
-        },
+        staticClass: 'btn',
+        class: self.classObject,
         on: {
           click: self.onClick
         }
       }, [self.$slots.default]);
 
+      if (self.tag) {
+        tagEl = c(self.tag, { class: this.tagClass ? this.tagClass : '' }, [buttonEl]);
+        return tagEl;
+      }
       return buttonEl;
+    },
+    props: {
+      tag: String,
+      tagClass: String,
+      primary: Boolean,
+      success: Boolean,
+      info: Boolean,
+      danger: Boolean,
+      warning: Boolean
+    },
+    computed: {
+      classObject () {
+        var self = this, co = {};
+        ['primary', 'success', 'info', 'danger', 'warning'].forEach(function (prop, index) {
+          if (self[prop]) co['btn-' + prop] = true;
+        });
+        return co;
+      }
     },
     methods: {
       onClick (event) {
